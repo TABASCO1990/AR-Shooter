ebabd8c55cb2f04a7b3763664475cb932279ddd2
Viacheslav Born <64132803+TABASCO1990@users.noreply.github.com>
2023-05-30 12:10:15 +0500

    Add Project

---
+++ AR Shooter/Assets/Scripts/Shooter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private Transform _shootPoint;
+    [SerializeField] private Bullet _bulletTemplate;
+
+    private void Update()
+    {
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(_bulletTemplate, _shootPoint);
+        }*/
+
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Instantiate(_bulletTemplate, _shootPoint);
+            }
+        }
+    }
+}
